176df8112089e35a3caba4e59cfca09128a90706
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-28 17:36:23 +0600

    fix somethng on greeting component

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -7,21 +7,21 @@ import Adhering from "../assets/home/greeting/adhering.svg";
 
 const Greeting = () => {
   return (
-    <div className="bg-[#F1F1EF] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 items-center h-[60rem] sm:h-[60rem] md:h-[39.375rem]">
-      <img className="mb-3 sm:mb-4 md:-mb-[4.6rem]" src={Sec} alt="section" />
-      <div className="text-center sm:text-center md:text-start">
-        <h1 className="text-3xl md:text-4xl lg:text-[2.625rem] mb-5">
+    <div className="bg-[#F1F1EF] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 items-center h-[60rem] sm:h-[60rem] md:h-[65rem] lg:h-[39.375rem] pb-10 sm:pb-10 md:pb-12 lg:pb-0">
+      <img className="mb-3 sm:mb-4 md:mb-10" src={Sec} alt="section" />
+      <div className="">
+        <h1 className="text-3xl md:text-4xl lg:text-[2.625rem] mb-5 ml-16 md:ml-24 lg:ml-0">
           Welcome to
           <br />
           Raghav BuildTech
         </h1>
-        <p className="text-[#404040] text-sm mb-[4.188rem] w-[45%]">
+        <p className="text-[#404040] text-sm mb-[4.188rem] w-[45%] ml-16 md:ml-24 lg:ml-0">
           Raghav BuildTech aspires to meet{" "}
           <span className="font-bold">on-demand repairs and renovation</span>{" "}
           requirements by drawing on their organizational skills & efficiency of
           handling the project.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 text-[#404040] w-[40%] sm:w-[50%] md:w-[65%] mx-auto sm:mx-auto md:mx-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 text-[#404040] w-[40%] sm:w-[50%] md:w-[65%] mx-auto sm:mx-auto md:mx-auto lg:mx-0">
           <div className="flex items-center justify-start">
             <img src={Utility} alt="restoration" />
             <p className="ml-6">Utility of tech enabled platform</p>
@@ -45,3 +45,6 @@ const Greeting = () => {
 };
 
 export default Greeting;
+
+// h-[60rem] sm:h-[60rem] md:h-[65rem] lg:h-[39.375rem]
+// items-center 
