42cfcf96263df2c88b10219f1bfdb91dd704a8b4
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-28 16:26:18 +0600

    fix testimonial and and carousel hero and testimonial section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SliderImg1 from "../assets/slider/sliderImg1.svg";
 import SliderImg2 from "../assets/slider/sliderImg2.svg";
 
+const slides = [
+  {
+    url: SliderImg1,
+  },
+  {
+    url: SliderImg2,
+  },
+];
+
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex(
+        currentIndex === slides.length - 1 ? 0 : currentIndex + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex]);
+
   return (
     <section>
       <div className="px-5 md:px-8 lg:px-[7.5rem] my-5 md:my-8 lg:my-[4.75rem]">
-        <h1 className="font-normal text-3xl md:text-4xl lg:text-[3.125rem] mb:3 md:mb-5 lg:mb-9">Turnkey Solution For Every Project</h1>
+        <h1 className="font-normal text-3xl md:text-4xl lg:text-[3.125rem] mb:3 md:mb-5 lg:mb-9">
+          Turnkey Solution For Every Project
+        </h1>
         <p className="text-sm text-[#545454] w-[100%] md:w-[70%] lg:w-[50%]">
           We are professionals who are experienced and deliver project of
           exceptional quality while staying on schedule and under budget.
         </p>
       </div>
       <div className="flex h-[20rem] md:h-[25rem] lg:h-[30.938rem]">
-        <img  width="100%" src={SliderImg1} alt="Hero image 1" />
-        {/* <img className="w-[20vw]" src={SliderImg2} alt="Hero image 2" /> */}
+        {slides.map((slide, index) => (
+          <img
+            width="100%"
+            key={index}
+            className={`absolute h-[20rem] md:h-[25rem] lg:h-[30.938rem] transition-all duration-1000 ${
+              currentIndex === index ? "left-0 opacity-100" : "opacity-0"
+            }`}
+            src={slide.url}
+            alt={`Hero image ${index + 1}`}
+          />
+        ))}
       </div>
     </section>
   );
--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SliderImg1 from "../assets/slider/sliderImg1.svg";
+import SliderImg2 from "../assets/slider/sliderImg2.svg";
+import SliderImg3 from "../assets/slider/sliderImg3.svg";
+
+const slides = [
+  {
+    url: SliderImg1,
+    author: "Abul",
+  },
+  {
+    url: SliderImg2,
+    author: "Babul",
+  },
+  {
+    url: SliderImg3,
+    author: "Cabul",
+  },
+];
 
 const Testimonial = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex(
+        currentIndex === slides.length - 1 ? 0 : currentIndex + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex]);
+
   return (
-    <div className="relative">
-      <img width="100%" src={SliderImg1} alt="gallery image" />
-      <div className="absolute bottom-[1rem] md:bottom-[3rem] lg:bottom-[5rem] xl:bottom-[7rem] 2xl:bottom-[9.563rem] top-[1rem] md:top-[3rem] lg:top-[5rem] xl:top-[5rem] xl:top-[9.563rem] left-[3rem] md:left-[5rem] lg:left-[10rem] xl:left-[12rem] 2xl:left-[14.063rem] pt-[1rem] md:pt-[3rem] lg:pt-[3.813rem] px-9 bg-white text-black w-[85%] md:w-[75%] lg:w-[50%] xl:w-[45%] 2xl:w-[30%]">
-        <p className="">I am delighted to see my refurbished home, transformed in a spectacular manner. The way the tiles have been laid, kitchen has been re-modelled and the hall has been given a face lift using best false ceiling concept with grouted LED lighting. Excellent execution and on time delivery. Kudos to RBT</p>
-        <p className="mt-3 md:mt-5 lg:mt-16"><span className="font-medium">Deepak</span> | <span className="text-[#545454]">Yelahanka</span></p>
+    <div className="relative bg-black">
+      <img
+        width="100%"
+        className="opacity-50 h-[39.375rem]"
+        src={slides[currentIndex].url}
+        alt="gallery image"
+      />
+
+      <div className="absolute bottom-[2rem] md:bottom-[5rem] lg:bottom-[8rem] top-[8rem] md:top-[12rem] lg:top-[8rem] left-[3rem] md:left-[5rem] lg:left-[10rem] xl:left-[12rem] 2xl:left-[14.063rem] pt-[4rem] px-9 bg-white text-black w-[85%] md:w-[60%] lg:w-[45%] xl:w-[45%] 2xl:w-[30%]">
+        <p className="">
+          I am delighted to see my refurbished home, transformed in a
+          spectacular manner. The way the tiles have been laid, kitchen has been
+          re-modelled and the hall has been given a face lift using best false
+          ceiling concept with grouted LED lighting. Excellent execution and on
+          time delivery. Kudos to RBT
+        </p>
+        <p className="mt-24 md:mt-16 lg:mt-16">
+          <span className="font-medium">{slides[currentIndex].author}</span> |{" "}
+          <span className="text-[#545454]">Yelahanka</span>
+        </p>
       </div>
     </div>
   );
