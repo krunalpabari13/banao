3fe50ac698348e962f0a6557ee2a1f3278d3552a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-30 14:25:28 +0600

    implement active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/header/Logo.svg";
 import MenuOpen from "../assets/header/menuOpen.svg";
 import SmLogo from "../assets/header/smLogo.svg";
@@ -18,25 +18,70 @@ const Navbar = () => {
         <img src={MenuOpen} alt="menuOpen" />
       </button>
       <Link to="/">
-        <img src={SmLogo} alt="Logo" className="lg:hidden " data-aos="zoom-in-right" />
-        <img src={Logo} alt="Logo" className="hidden lg:block" data-aos="zoom-in" />
+        <img
+          src={SmLogo}
+          alt="Logo"
+          className="lg:hidden "
+          data-aos="zoom-in-right"
+        />
+        <img
+          src={Logo}
+          alt="Logo"
+          className="hidden lg:block"
+          data-aos="zoom-in"
+        />
       </Link>
       <div className="flex">
-        <ul className="hidden lg:flex items-center me-auto ">
-          <li className="md:mx-5 uppercase text-[10px] lg:text-sm">
-            <Link to="/">Home</Link>
+        <ul className="hidden lg:flex items-center me-auto">
+          <li className="uppercase text-[10px] lg:text-sm">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] py-[1.35rem] md:px-5" : "md:px-5"
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
           </li>
-          <li className="md:mx-5 uppercase text-[10px] lg:text-sm">
-            <Link to="/services">Services</Link>
+          <li className="uppercase text-[10px] lg:text-sm">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] py-[1.35rem] md:px-5" : "md:px-5"
+              }
+              to="/services"
+            >
+              Services
+            </NavLink>
           </li>
-          <li className="md:mx-5 uppercase text-[10px] lg:text-sm">
-            <Link to="/gallery">Gallery</Link>
+          <li className="uppercase text-[10px] lg:text-sm">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] py-[1.35rem] md:px-5" : "md:px-5"
+              }
+              to="/gallery"
+            >
+              Gallery
+            </NavLink>
           </li>
-          <li className="md:mx-5 uppercase text-[10px] lg:text-sm">
-            <Link to="/about">About Us</Link>
+          <li className="uppercase text-[10px] lg:text-sm">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] py-[1.35rem] md:px-5" : "md:px-5"
+              }
+              to="/about"
+            >
+              About Us
+            </NavLink>
           </li>
-          <li className="md:mx-5 uppercase text-[10px] lg:text-sm">
-            <Link to="/contact">Contact</Link>
+          <li className="uppercase text-[10px] lg:text-sm">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] py-[1.35rem] md:px-5" : "md:px-5"
+              }
+              to="/contact"
+            >
+              Contact
+            </NavLink>
           </li>
         </ul>
         <button className="px-5 text-[10px] lg:text-sm text-white uppercase bg-[#CD4055] h-[56px] md:h-[65px]">
@@ -49,20 +94,70 @@ const Navbar = () => {
           <li className="mx-6 py-3 flex justify-end">
             <img onClick={toggleMenu} src={MenuClose} alt="menuClose" />
           </li>
-          <li onClick={toggleMenu} className="mx-6 py-3 uppercase text-[10px] lg:text-sm">
-            <Link to="/">Home</Link>
+          <li
+            onClick={toggleMenu}
+            className="uppercase text-[10px] lg:text-sm my-5"
+          >
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] px-6 py-3" : "px-6 py-3"
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
           </li>
-          <li onClick={toggleMenu} className="mx-6 py-3 uppercase text-[10px] lg:text-sm">
-            <Link to="/services">Services</Link>
+          <li
+            onClick={toggleMenu}
+            className="my-5 uppercase text-[10px] lg:text-sm"
+          >
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] px-6 py-3" : "px-6 py-3"
+              }
+              to="/services"
+            >
+              Services
+            </NavLink>
           </li>
-          <li onClick={toggleMenu} className="mx-6 py-3 uppercase text-[10px] lg:text-sm">
-            <Link to="/gallery">Gallery</Link>
+          <li
+            onClick={toggleMenu}
+            className="my-5 uppercase text-[10px] lg:text-sm"
+          >
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] px-6 py-3" : "px-6 py-3"
+              }
+              to="/gallery"
+            >
+              Gallery
+            </NavLink>
           </li>
-          <li onClick={toggleMenu} className="mx-6 py-3 uppercase text-[10px] lg:text-sm">
-            <Link to="/about">About Us</Link>
+          <li
+            onClick={toggleMenu}
+            className="my-5 uppercase text-[10px] lg:text-sm"
+          >
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] px-6 py-3" : "px-6 py-3"
+              }
+              to="/about"
+            >
+              About Us
+            </NavLink>
           </li>
-          <li onClick={toggleMenu} className="mx-6 py-3 uppercase text-[10px] lg:text-sm">
-            <Link to="/contact">Contact</Link>
+          <li
+            onClick={toggleMenu}
+            className="my-5 uppercase text-[10px] lg:text-sm"
+          >
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "bg-[#EEEEEE] px-6 py-3" : "px-6 py-3"
+              }
+              to="/contact"
+            >
+              Contact
+            </NavLink>
           </li>
         </ul>
       )}
