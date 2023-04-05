02c594ec24513c28cb15d9be97ee1080103ed1ae
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-05 14:28:33 +0600

    client message added

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
 import SliderImg1 from "../assets/slider/sliderImg1.svg";
 import SliderImg2 from "../assets/slider/sliderImg2.svg";
-import SliderImg3 from "../assets/slider/sliderImg3.svg";
+import SliderImg4 from "../assets/slider/sliderImage4.png";
 
 const slides = [
   {
     url: SliderImg1,
     author: "Deepak",
+    message:
+      "I am delighted to see my refurbished home, transformed in a spectacular manner. The way the tiles have been laid, kitchen has been re-modelled and the hall has been given a face lift using best false ceiling concept with grouted LED lighting. Excellent execution and on time delivery. Kudos to RBT",
   },
   {
     url: SliderImg2,
-    author: "Deepak",
+    author: "BALAJI",
+    message: "We should thank you for the repairs and maintenece work done by you and your team and handing over the house in order on time."
   },
   {
-    url: SliderImg3,
-    author: "Deepak",
+    url: SliderImg4,
+    author: "Kalaivani",
+    message: "We will definitely call you only again because we will be and we are confident that out house is in  safe hands."
   },
 ];
 
@@ -40,14 +44,11 @@ const Testimonial = () => {
         alt="gallery image"
       />
 
-      <div className="absolute bottom-[2rem] md:bottom-[5rem] lg:bottom-[8rem] top-[8rem] md:top-[12rem] lg:top-[8rem] left-[3rem] md:left-[5rem] lg:left-[10rem] xl:left-[12rem] 2xl:left-[14.063rem] pt-[4rem] px-9 bg-white text-black w-[85%] md:w-[60%] lg:w-[45%] xl:w-[45%] 2xl:w-[30%]" data-aos="fade-right">
-        <p className="">
-          I am delighted to see my refurbished home, transformed in a
-          spectacular manner. The way the tiles have been laid, kitchen has been
-          re-modelled and the hall has been given a face lift using best false
-          ceiling concept with grouted LED lighting. Excellent execution and on
-          time delivery. Kudos to RBT
-        </p>
+      <div
+        className="absolute bottom-[2rem] md:bottom-[5rem] lg:bottom-[8rem] top-[8rem] md:top-[12rem] lg:top-[8rem] left-[3rem] md:left-[5rem] lg:left-[10rem] xl:left-[12rem] 2xl:left-[14.063rem] pt-[4rem] px-9 bg-white text-black w-[85%] md:w-[60%] lg:w-[45%] xl:w-[45%] 2xl:w-[30%]"
+        data-aos="fade-right"
+      >
+        <p className="">{slides[currentIndex].message}</p>
         <p className="mt-24 md:mt-16 lg:mt-16">
           <span className="font-medium">{slides[currentIndex].author}</span> |{" "}
           <span className="text-[#545454]">Yelahanka</span>
