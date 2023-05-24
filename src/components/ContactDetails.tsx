5f1dfe82f2a3b2ac45fd5be2eb57abcf0f5de90c
amshal1 <panshal911@gmail.com>
2023-05-24 12:40:51 +0530

    Removed unused comments

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -14,9 +14,6 @@ const ContactDetails = () => {
         <p className="pl-8 text-sm font-normal">
           <a href="tel:+918754502338">+91 875-450-2338</a>
         </p>
-        {/* <p className="pl-8 text-sm font-normal">
-          <a href="tel:+918754502338">+91 875-450-2338</a>
-        </p> */}
       </div>
       <div className="w-[280px] mt-5 lg:mt-0" data-aos="zoom-in">
         <div className="flex items-center pb-3">
@@ -36,9 +33,6 @@ const ContactDetails = () => {
         <p className="pl-8 text-sm font-normal">
           <a href="mailto:ragainn@gmail.com">ragainn@gmail.com</a>
         </p>
-        {/* <p className="pl-8 text-sm font-normal">
-          <a href="mailto:ragainn@gmail.com">ragainn@gmail.com</a>
-        </p> */}
       </div>
     </div>
   );
